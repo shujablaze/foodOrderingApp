d7a88691ad0cda93d5a3a37249b043a9f2c4cab7
shujablaze <shujahamid7@gmail.com>
2021-08-23 23:25:11 +0530

    added file upload

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,4 +1,17 @@
+const multer = require('multer');
 const Category = require('../Models/Category')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+  
+const upload = multer({ storage: storage })
 
 exports.newCategory = async (req,res,next) => {
     try{
--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -1,6 +1,18 @@
 const { findOneAndUpdate, findByIdAndDelete } = require('../Models/Item')
 const Item = require('../Models/Item')
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+  
+const upload = multer({ storage: storage })
+
 exports.newItem = async (req,res,next) => {
     try{
         const newItem = await Item.create(req.body)
